1a09d018330a52df8f5afc9be5b115d73dc2bb77
nhunganh4502 <nhunganh4502@gmail.com>
2023-12-06 22:54:31 +0700

    update

---
+++ src/Test2/Client.java
@@ -0,0 +1,59 @@
+package Test2;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        String serverAddress = "localhost"; // Change this to the IP or hostname of the server if needed
+        int serverPort = 10006; // Use the server port
+
+        try (DatagramSocket socket = new DatagramSocket();
+             Scanner scanner = new Scanner(System.in)) {
+            InetAddress serverIPAddress = InetAddress.getByName(serverAddress);
+
+            // Step 1: Send Student_ID and receive the first server response
+            String studentID = "2001040006"; // Replace with your student ID
+            sendRequest(socket, studentID, serverIPAddress, serverPort);
+            String serverResponse = receiveResponse(socket);
+            System.out.println("Received from server: " + serverResponse);
+
+            // Step 2: Continue to read and send messages
+            while (true) {
+                System.out.print("Enter a message: ");
+                String clientMessage = scanner.nextLine();
+                sendRequest(socket, clientMessage, serverIPAddress, serverPort);
+
+                // Step 3: Handle server responses
+                serverResponse = receiveResponse(socket);
+                System.out.println("Received from server: " + serverResponse);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendRequest(DatagramSocket socket, String message, InetAddress serverAddress, int serverPort) {
+        try {
+            byte[] sendData = message.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+            socket.send(sendPacket);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String receiveResponse(DatagramSocket socket) {
+        try {
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            socket.receive(receivePacket);
+            return new String(receivePacket.getData(), 0, receivePacket.getLength());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
---
+++ src/Test2/Server.java
@@ -0,0 +1,47 @@
+package Test2;
+import java.math.BigInteger;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Server {
+    public static void main(String[] args) {
+        int port = 10006; // Use the last 3 digits of your student ID
+        try (DatagramSocket serverSocket = new DatagramSocket(port)) {
+            System.out.println("Server is running on port " + port);
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+
+            String studentID = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Received from client: " + studentID);
+
+            BigInteger studentIDValue = new BigInteger(studentID);
+            BigInteger result = studentIDValue.multiply(BigInteger.valueOf(3));
+            byte[] sendData = result.toString().getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+            serverSocket.send(sendPacket);
+
+            while (true) {
+                receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Received from client: " + clientMessage);
+
+                if (clientMessage.matches("\\d+")) {
+                    BigInteger number = new BigInteger(clientMessage);
+                    BigInteger cubeResult = number.pow(3);
+                    sendData = cubeResult.toString().getBytes();
+                } else {
+                    sendData = clientMessage.getBytes();
+                }
+
+                sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+                serverSocket.send(sendPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
